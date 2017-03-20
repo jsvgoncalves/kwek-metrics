8acfb0e493dbd73c0cc091ef26b3e54f5bf7ec12
João Gonçalves <jsvgoncalves@gmail.com>
2017-03-20 15:35:21 +0100

    wip: kwek: new endpoints
    * Get data using new endpoints
    
    Signed-off-by: Joao Goncalves <jsvgoncalves@gmail.com>

--- api/hawkular.py
+++ api/hawkular.py
@@ -3,8 +3,6 @@
 # This file is part of Kwek Metrics.
 
 """Hawkular API calls."""
-
-
 import json
 
 import requests
@@ -18,8 +16,10 @@ if DISABLE_WARNINGS:
 
 
 class HawkularAPIError(Exception):
-    def __init__(self, message):
+    """Custom Exception type for API Errors.
+    """
 
+    def __init__(self, message):
         # Call the base class constructor with the parameters it needs
         super(HawkularAPIError, self).__init__(message)
 
@@ -48,14 +48,15 @@ def _build_hawkular_headers(tentant="", auth=""):
     return{
         'Hawkular-Tenant': "{0}".format(tentant),
         'Accept': 'application/json',
-        'Authorization': 'Bearer {0}'.format(auth)
+        'Authorization': 'Bearer {0}'.format(auth),
+        'Content-Type': 'application/json'
     }
 
 
 def _build_hawkular_payload(metric):
     """Generate the payload for the current select metric.
 
-    {'tags': 'descriptor_name:memory%2Fusage',
+    {'tags': 'descriptor_name:memory/usage',
      'bucketDuration': '60000ms',
      'start': '-15mn',
      'stacked': 'true'}
@@ -74,11 +75,76 @@ def _build_hawkular_payload(metric):
     }
 
 
+def _build_hawkular_tags_payload(tags, metric_type):
+    """Generate the payload for the current select metric.
+
+    Example:
+    {'tags': 'descriptor_name:memory/usage|cpu/usage_rate',
+     'buckets': '1',
+     'type': 'pod_container'
+    }
+
+    Args:
+        tags (list): The string describing the metric on the API
+        metric_type (str): The "type" for the payload, either
+            "pod" or "pod_container"
+
+    Returns:
+        dict: A dict with formatted payload
+    """
+    return{
+        'buckets': '1',
+        'tags': 'descriptor_name:{},type:{}'.format(
+            '|'.join(tags),
+            metric_type)
+    }
+
+
 def get_metric(url, tenant, auth, metric):
+    """Get a metric dataset from Hawkular API.
+
+    Args:
+        url (str): The Hawkular endpoint to query
+        tenant (str): The tenant with which to authenticate
+        auth (str): The token for authentication
+        metric (str): The desired metric
+
+    Returns:
+        str: The text from the processed request
+
+    Raises:
+        err: (ValueError, ConnectionError)
+    """
     headers = _build_hawkular_headers(tenant, auth)
     payload = _build_hawkular_payload(metric)
     try:
         r = query_api(url, headers, payload)
+        return json.loads(r.text)
+    except (ValueError, ConnectionError) as err:
+        raise err
+
+
+def get_metrics(url, tenant, auth, tags, pod_selectors='pod'):
+    """Get multiple metrics datasets from Hawkular API.
+
+    Args:
+        url (str): The Hawkular endpoint to query
+        tenant (str): The tenant with which to authenticate
+        auth (str): The token for authentication
+        tags (list): The tags to query the endpoint
+        pod_selectors (str, optional): The "type" for the payload,
+            either "pod" or "pod_container"
+
+     Returns:
+        str: The text from the processed request
+
+    Raises:
+        err: (ValueError, ConnectionError)
+    """
+    headers = _build_hawkular_headers(tenant, auth)
+    payload = _build_hawkular_tags_payload(tags, pod_selectors)
+    try:
+        r = query_api_post(url, headers, payload)
         return json.loads(r.text)
     except (ValueError, ConnectionError) as err:
         raise err
@@ -92,7 +158,6 @@ def _build_os_headers(auth=""):
      'Authorization': 'Bearer XXXX'}
 
     Args:
-        tentant (str): The tenant which to query
         auth (str): The authorization token
 
     Returns:
@@ -104,6 +169,18 @@ def _build_os_headers(auth=""):
 
 
 def get_os_projects(url, auth):
+    """Get the visible OpenShift projects from OpenShift API.
+
+    Args:
+        url (str): The endpoint of OpenShift API
+        auth (str): The token to authenticate
+
+    Returns:
+        str: The JSON formatted text of the available projects
+
+    Raises:
+        err: (ValueError, ConnectionError)
+    """
     headers = _build_os_headers(auth)
     try:
         r = query_api(url, headers)
@@ -129,6 +206,29 @@ def query_api(url, headers={}, payload={}):
     """
     try:
         r = requests.get(url, headers=headers, verify=False, params=payload)
+        if r.status_code == 401:
+            raise APIAuthorizationError('Unauthorized')
+        return r
+    except ConnectionError as err:
+        raise err
+
+
+def query_api_post(url, headers={}, payload={}):
+    """Perfoms a GET request.
+
+    Args:
+        url (str): The url to perfom the GET
+        headers (dict, optional): The headers for the request
+        payload (dict, optional): The payload for the request
+
+    Returns:
+        Request: The Request object
+
+    Raises:
+        err: ConnectionError raised by requests.get()
+    """
+    try:
+        r = requests.post(url, headers=headers, verify=False, json=payload)
         if r.status_code == 401:
             raise APIAuthorizationError('Unauthorized')
         return r
--- kwek/views/views.py
+++ kwek/views/views.py
@@ -11,6 +11,8 @@ from requests import ConnectionError
 
 from api.hawkular import get_os_projects
 from api.hawkular import get_metric
+from api.hawkular import get_metrics
+from api.hawkular import APIAuthorizationError
 from kwek.models import Service, Metric
 
 
@@ -20,9 +22,6 @@ blueprint = Blueprint('kwek', __name__,
 
 @blueprint.route('/', methods=['GET'])
 def index():
-    """Home page.
-    Return a list of all the projects and it's top-level metrics.
-    """
     s = Service.query.filter_by().first()
     if s is None:
         flash('No Service found. Please add one below.', 'danger')
@@ -34,42 +33,79 @@ def index():
     except (ValueError, AttributeError, ConnectionError) as err:
         projects = {}
         flash(err.message, 'danger')
-    metrics = Metric.query.all()
+    except APIAuthorizationError as err:
+        flash(err.message, 'danger')
+        return redirect(url_for('services.index'))
+    # Build the query string
+    # https://metrics.engint.openshift.com/hawkular/metrics/metrics/stats/query
+    # {"tags":"descriptor_name:network/tx_rate|network/rx_rate,type:pod","buckets":"1"}
+    # {"tags":"descriptor_name:memory/usage|cpu/usage_rate,type:pod_container","buckets":"1"}
+    metrics_pod = {
+        'endpoint': 'metrics/stats/query',
+        'type': 'pod_container',
+        'queries': [
+            {'conversion': 9.53674e-07,
+             'display_name': 'Memory Usage',
+             'name': 'memory', 'color': 'blue', 'tag': 'memory/usage',
+             'id': 2, 'unit': 'MiB'},
+            {'conversion': 1.0,
+             'display_name': 'CPU Usage',
+             'name': 'cpu', 'color': 'red', 'tag': 'cpu/usage_rate',
+             'id': 2, 'unit': 'Millicores'}
+        ]
+    }
 
-    # Get the values for each metric, per project
-    values = {}
-    for project in projects:
-        values[project['metadata']['name']] = {}
-        try:
-            for metric in metrics:
-                v = get_metric(
-                    urljoin(s.hwk_url, metric.endpoint),
-                    project['metadata']['name'],
-                    s.token,
-                    metric.tag)
-                values[project['metadata']['name']][metric.name] = v
-        except (ValueError, KeyError, ConnectionError) as err:
-            flash(err.message, 'danger')
-
-    # Calculate the aggregated value of each metric
+    # Initialize the metric totals
     totals = {}
-    for metric in metrics:
-        totals[metric.name] = 0
-        for project in projects:
-            try:
-                # Get the last measure for the given metric
-                last_measure = values[
-                    project['metadata']['name']][metric.name][0]
-                # And sum it to our total per metric
-                totals[metric.name] += last_measure['avg']
-            except KeyError as err:
-                flash(err.message, 'danger')
+    # And the tags for the API request
+    tags = []
+    for metric in metrics_pod['queries']:
+        tags.append(metric['tag'])
+        totals[metric['tag']] = 0
+
+    # Initialize metric values dict
+    values = {}
+    for p in projects:
+        # Initialize metric for the project
+        name = p['metadata']['name']
+        values[name] = {'avg': {}}
+        for tag in tags:
+            # Initialize metric tags for the project
+            values[name]['avg'][tag] = 0
+            values[name][tag] = []
+        try:
+            # Get all the metrics for the project
+            v = get_metrics(
+                urljoin(s.hwk_url, metrics_pod['endpoint']),
+                name,
+                s.token,
+                tags,
+                metrics_pod['type'])
+            # Iterate over the values
+            for k, v in v['gauge'].iteritems():
+                # Filter out `sti-build` values, e.g.:
+                # "sti-build/7c197071-193a-11e7-8dbe-126ee6b1d97f/cpu/usage_rate"
+                if k.partition('/')[0] != 'sti-build':
+                    # Filter out Empty values
+                    if v[0]['empty'] is False:
+                        # Append each metric to its proper tag
+                        for tag in tags:
+                            if tag in k:
+                                values[name][tag].append(v)
+                                values[name][
+                                    'avg'][tag] += v[0]['avg']
+                                totals[tag] += v[0]['avg']
+        except Exception as err:
+            flash('{} - {}'.format(name, err.message), 'danger')
+
+    for k, v in values.iteritems():
+        pass
 
     return render_template(
         'index.html',
         projects=projects,
-        metrics=metrics,
         values=values,
+        metrics=metrics_pod['queries'],
         totals=totals)
 
 
